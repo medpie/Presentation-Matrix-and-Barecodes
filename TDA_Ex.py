0ba0892f46897978f7155a78cbe62ebe748415e6
PepCoola <127675274+medpie@users.noreply.github.com>
2023-11-19 18:48:09 -0600

    Add files via upload

--- TDA_Ex.py
+++ TDA_Ex.py
@@ -50,17 +50,61 @@ def barcode(matrix, m, n):
   for k in range(n):
     if (pivot(x,m,n)[k] != None) and (pivot(x,m,n)[k] < k+1):
           barc.append(f'[{pivot(x,m,n)[k]}, {k+1})')
-    elif (pivot(x,m,n)[k] == None) and ((k+1)) not in pivot(x,m,n):
+    elif (pivot(x,m,n)[k] == None) and ((k+1) not in pivot(x,m,n)):
         barc.append(f'[{k+1}, infinity)')
   return barc
 
 
-
+def bar(matrix, m, n):
+  #Choose the type of standard reduction based on the abient field
+  x = standard_breduced(matrix,m,n)
+  barc = []
+  for k in range(n):
+    if (pivot(x,m,n)[k] != None) and (pivot(x,m,n)[k] < k+1):
+          barc.append([pivot(x,m,n)[k], k+1])
+    elif (pivot(x,m,n)[k] == None) and ((k+1) not in pivot(x,m,n)):
+        barc.append([k+1])
+  return barc
 
 
 #matrix = [[random.randint(0,1) for _ in range(10)] for _ in range(10)]
+#m = [[0, 0, 0, 0, 0, 0, 0, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 1, 1, 0, 1, 0, 1], [0, 0, 0, 0, 1, 1, 0, 1, 1, 1], [0, 1, 0, 1, 1, 1, 0, 0, 1, 0], [0, 0, 1, 1, 0, 1, 1, 1, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0, 1, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 0, 1, 1, 1, 0, 1, 0], [1, 1, 0, 0, 0, 1, 0, 0, 1, 1]]
+
+#print(matrix)
 #print(matrix)
 #print(pivot(matrix,10,10))
-#standard_breduced(matrix,10,10)
+#standard_reduced(matrix,10,10)
 #print(pivot(matrix,10,10))
-#print(barcode(matrix, 10,10))
+#print(barcode_list(m, 10,10))
+#print(barcode(m, 10, 10))
+
+# Making a table of square matrix dimensions and their reduction runtimes
+"""
+data = [[random.randint(0,1) for _ in range(10)] for _ in range(10)]
+x = [[np. for _ in range(10)] for _ in range(10)]
+print(standard_reduced(data,10,10))
+print(pivot(data,10,10))
+
+values = []
+for dim in range(5,30,5):
+  a = t.time()
+  matrix = [[random.randint(0,1) for _ in range(dim)] for _ in range(dim)]
+  #matrix = [[1, 0, 0, 1, 0], [0, 0, 0, 0, 0], [1, 0, 0, 1, 0], [1, 1, 0, 1, 0], [1, 1, 0, 0, 1]]
+  #print(matrix)
+  #print(f'Pivots of the original matrix: {pivot(matrix, dim,dim)}')
+  reduced_matrix = standard_reduced(matrix,dim,dim)
+  #print(reduced_matrix)
+  print(f'Pivots of the reduced matrix of dimension {dim}*{dim}: {pivot(reduced_matrix, dim,dim)}')
+#print(len(set(pivot(matrix,10,10))))            
+  b=t.time()
+  values.append([f'{dim}*{dim}', f'{round((b-a)*1000, 2)} ms'])
+
+dictionary = dict(zip([_ for _ in 'ABCDE'],values))
+df = pd.DataFrame(dictionary)
+df.index = ['Dimesnion', 'Runtime']
+print(df) 
+"""
+
+
+
+
