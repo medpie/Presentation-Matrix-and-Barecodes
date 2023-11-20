6b0e36805b7e5ba7ed330b7b99762514600ab978
PepCoola <127675274+medpie@users.noreply.github.com>
2023-11-19 18:49:45 -0600

    Add files via upload
    # Generating a dimension-sorted dictionary of simplices from a simplicial complex.
    # Creating a list of simplices from a simplicial complex
    # Creating the boundary matrix of a simplicial complex
    # Plotting Persistence Diagram

--- complex2Matrix.py
+++ complex2Matrix.py
@@ -1,7 +1,8 @@
 # Generating a dimension-sorted dictionary of simplices from a simplicial complex.
 # Creating a list of simplices from a simplicial complex
 # Creating the boundary matrix of a simplicial complex
-
+# Plotting Persistence Diagram 
+import matplotlib.pyplot as plt
 import TDA_Ex as tdx
 
 def complex_dimension(scomplex):
@@ -79,22 +80,50 @@ def matrixof_simplices(scomplex):
            matrix[i][j] = 1
 
   return matrix
+
+#Persistence Diagram
+def pers_diagram(scomplex):
+     n = len(listof_simplices(scomplex))
+     list = tdx.bar(matrixof_simplices(scomplex), n, n)
+     x, y = [], []
+     for point in list:
+        if len(point) == 2:
+           x.append(point[0])
+           y.append(point[1])
+        else:
+           point.append(n)
+           x.append(point[0])
+           y.append(n+3)
+     plt.scatter(x,y)
+     plt.plot([0,n], [0,n], '--')
+     plt.xticks(range(1,n+1))
+     plt.yticks(range(1,n+1))
+     plt.title('Persistence Diagram')
+     plt.text(0, n+4, '--infinity--', fontsize=10, color='red')
+     plt.text(2, 0, f'Complex: {scomplex}' , fontsize=10, color='green')
+     plt.show()
   
   
+     
+     
 
 
 #scomplex = [[1,2,3],[3,4],[4,5,6,7],[4,1]]
-scomplex = [[1,2,3]]
+scomplex = [[1,2],[1,3],[2,3,4],[4,5],[4,6],[5,6],[6,7]]
+#scomplex = [[1,2,3]]
 #print(dict_simplices(scomplex))
 n = len(listof_simplices(scomplex))
 #print(listof_simplices(scomplex).get(3))
-print(tdx.standard_breduced(matrixof_simplices(scomplex), n, n))
-print(tdx.barcode(matrixof_simplices(scomplex),n,n))
+#print(tdx.standard_breduced(matrixof_simplices(scomplex), n, n))
 #print(tdx.pivot(matrixof_simplices(scomplex), n,n))
-x = matrixof_simplices(scomplex)
-y = tdx.standard_breduced(x, n,n)
-z = tdx.pivot(y, n,n)
-print(z)
+#x = matrixof_simplices(scomplex)
+#y = tdx.standard_breduced(x, n,n)
+#z = tdx.pivot(y, n,n)
+#print(z)
+#print(tdx.bar(matrixof_simplices(scomplex),n,n))
+#print(tdx.barcode(matrixof_simplices(scomplex),n,n))
+#print(pers_diagram(scomplex))
+pers_diagram(scomplex)
 
 
 
