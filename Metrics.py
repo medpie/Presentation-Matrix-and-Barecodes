e05b6e240f5b5df0b23d179bf5703b17d2814966
PepCoola <127675274+medpie@users.noreply.github.com>
2023-11-25 17:46:24 -0600

    Add files via upload
    Wasserstein distance of two barcodes (two list of intervals)

---
+++ Metrics.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+def dist(I, J, r):
+  distance, list2 = 0, []
+  for i in range(len(I)):
+    for j in range(len(J)):
+            if np.abs(I[i][0] - J[j][0]) <= r and np.abs(I[i][1] - J[j][1]) <= r:
+              distance += np.abs(I[i][0] - J[j][0]) + np.abs(I[i][1] - J[j][1])
+            else:
+               distance += 2*np.abs(I[i][0] - (I[i][0] + I[i][1])/2) + 2*np.abs(J[j][0] - (J[j][0] + J[j][1])/2)
+  return distance  
+        
+def Wasserstein(I, J):
+   max_delta = max([max([I[_][0] for _ in range(len(I))]), max([I[_][1] for _ in range(len(I))]), max([J[_][0] for _ in range(len(J))]), max([J[_][1] for _ in range(len(J))])])
+   distances = [dist(I, J, r) for r in range(1, max_delta + 1)]
+   index = distances.index(min(distances))
+   return index + 1
+
+x = [[random.randint(0,20), random.randint(0,20)] for _ in range(random.randint(1,20))]
+print(f'Barcode1: {x}')
+y = [[random.randint(0,20), random.randint(0,20)] for _ in range(random.randint(1,20))]
+print(f'Barcode2: {y}')
+R = [_ for _ in range(20)]
+Dist = [dist(x,y,R[_]) for _ in range(20)]
+plt.scatter(R, Dist) 
+plt.show()
+
+print(f'Wasserstein Distance: {Wasserstein(x,y)}')
+      
+
+
+
+      
+          
