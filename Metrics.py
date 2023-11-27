58da55e6f76f28a90537a05cc5709bb8bcf0d89d
PepCoola <127675274+medpie@users.noreply.github.com>
2023-11-26 22:12:45 -0600

    Add files via upload
    Computing Wasserstein distance of two barcodes (i.e., two lists of birth-death intervals) and the minimum delta at which the distance is attained.

--- Metrics.py
+++ Metrics.py
@@ -12,23 +12,23 @@ def dist(I, J, r):
                distance += 2*np.abs(I[i][0] - (I[i][0] + I[i][1])/2) + 2*np.abs(J[j][0] - (J[j][0] + J[j][1])/2)
   return distance  
         
-def Wasserstein(I, J):
+def Wasserstein_delta(I, J):
    max_delta = max([max([I[_][0] for _ in range(len(I))]), max([I[_][1] for _ in range(len(I))]), max([J[_][0] for _ in range(len(J))]), max([J[_][1] for _ in range(len(J))])])
    distances = [dist(I, J, r) for r in range(1, max_delta + 1)]
    index = distances.index(min(distances))
    return index + 1
 
-x = [[random.randint(0,20), random.randint(0,20)] for _ in range(random.randint(1,20))]
+x = [[random.randint(0,30), random.randint(0,30)] for _ in range(random.randint(1,30))]
 print(f'Barcode1: {x}')
-y = [[random.randint(0,20), random.randint(0,20)] for _ in range(random.randint(1,20))]
+y = [[random.randint(0,30), random.randint(0,30)] for _ in range(random.randint(1,30))]
 print(f'Barcode2: {y}')
-R = [_ for _ in range(20)]
-Dist = [dist(x,y,R[_]) for _ in range(20)]
+R = [_ for _ in range(30)]
+Dist = [dist(x,y,R[_]) for _ in range(30)]
 plt.scatter(R, Dist) 
 plt.show()
 
-print(f'Wasserstein Distance: {Wasserstein(x,y)}')
-      
+print(f'Wasserstein distance = {min(Dist)},  delta = {Wasserstein_delta(x,y)}')
+
 
 
 
