b90369eccd15fe86def6beadc18962ae8ea5b2b6
PepCoola <127675274+medpie@users.noreply.github.com>
2023-11-28 16:33:46 -0600

    Metrics.py
    Computing/illustrating Wasserstein distance of two barcodes (i.e., two lists of birth-death intervals)

--- Metrics.py
+++ Metrics.py
@@ -3,13 +3,13 @@ import random
 import matplotlib.pyplot as plt
 
 def dist(I, J, r):
-  distance, list2 = 0, []
+  distance = 0
   for i in range(len(I)):
     for j in range(len(J)):
             if np.abs(I[i][0] - J[j][0]) <= r and np.abs(I[i][1] - J[j][1]) <= r:
               distance += np.abs(I[i][0] - J[j][0]) + np.abs(I[i][1] - J[j][1])
             else:
-               distance += 2*np.abs(I[i][0] - (I[i][0] + I[i][1])/2) + 2*np.abs(J[j][0] - (J[j][0] + J[j][1])/2)
+               distance += 2*(np.abs(I[i][0] - (I[i][0] + I[i][1])/2) + np.abs(J[j][0] - (J[j][0] + J[j][1])/2))
   return distance  
         
 def Wasserstein_delta(I, J):
@@ -24,13 +24,9 @@ y = [[random.randint(0,30), random.randint(0,30)] for _ in range(random.randint(
 print(f'Barcode2: {y}')
 R = [_ for _ in range(30)]
 Dist = [dist(x,y,R[_]) for _ in range(30)]
+ax = plt.subplot()
+ax.set_xlabel('delta', color = 'red')
+ax.set_ylabel('Distance(delta)', color = 'red')
+ax.set_title(f'Wasserstein distance = {min(Dist)} is attained at delta = {Wasserstein_delta(x,y)}', ha = 'center', color = 'red')
 plt.scatter(R, Dist) 
 plt.show()
-
-print(f'Wasserstein distance = {min(Dist)},  delta = {Wasserstein_delta(x,y)}')
-
-
-
-
-      
-          
