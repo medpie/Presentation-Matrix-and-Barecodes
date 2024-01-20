ed80155db78a780fcbbecc793d88f8f7f8d0cd2c
PepCoola <127675274+medpie@users.noreply.github.com>
2024-01-19 23:14:43 -0600

    complex2Matrix.py

--- complex2Matrix.py
+++ complex2Matrix.py
@@ -6,39 +6,85 @@ import matplotlib.pyplot as plt
 import TDA_Ex as tdx
 import numpy as np
 from itertools import combinations
-  
 
+# List of simplices
 def listof_simplices(scomplex):
-  simplices = []
-  for s in scomplex:
-     dim_s = len(s)
-     for index in range(1,dim_s + 1):
-        sim_index = [obj for obj in combinations(s, index)]
-        simplices += sim_index
-  simplices.sort(key = len)
-  return list(set(simplices))
+  simplices = set([comb for sim in scomplex for index in range(1,len(sim) + 1) for comb in combinations(sim, index)])
+  return sorted(list(simplices), key = len)
+
+#dictionary of simplices with keys = dimensions
+def dictof_simplices(scomplex):
+  simplices = listof_simplices(scomplex)   
+  key = [dim for dim in range(max([len(sim) for sim in scomplex]))] # dimensions of simplices
+  value = [[] for _ in range(len(key))]
+  for item in simplices:
+     index = len(item) - 1
+     value[index].append(item)
+  return dict(zip(key,value))
+
 
 
 #=========================================
 def matrixof_simplices(scomplex):
-  n = len(listof_simplices(scomplex))
+  dic = dictof_simplices(scomplex)
+  dimension = max(dic.keys())
+  allSimplices = [sim for k in range(dimension + 1) for sim in dic[k]]
+  n = len(allSimplices)
   matrix = np.array([[0 for _ in range(n)] for _ in range(n)])
-  for simplex in listof_simplices(scomplex):
-     dim_simplex = len(simplex) - 1
-     if dim_simplex > 0:
-        j = listof_simplices(scomplex).index(simplex)
-        lower_d = [obj for obj in combinations(simplex, dim_simplex)]
-        for num in range(len(lower_d)):
-           i = listof_simplices(scomplex).index(lower_d[num])
+  key = 1
+  while key <= dimension:
+   for simplex in dic[key]:
+      j = allSimplices.index(simplex)
+      for lower_d in dic[key -1]:
+         i = allSimplices.index(lower_d)
+         if set(lower_d).issubset(simplex) == True:
            matrix[i,j] = 1
-  return matrix
+   key += 1
+   if key == dimension + 1:
+      break
+  return matrix       
 
+
+#print(dictof_simplices(scomplex))
+#print(matrixof_simplices(scomplex))
+
+#=============With Clearing
+
+def Cmatrixof_simplices(scomplex):
+  dic = dictof_simplices(scomplex)
+  dimension = max(dic.keys())
+  allSimplices = [sim for k in range(dimension + 1) for sim in dic[k]]
+  n = len(allSimplices)
+  matrix = np.array([[0 for _ in range(n)] for _ in range(n)])
+  key = 1
+  while key <= dimension:
+   for simplex in dic[key]:
+      j = allSimplices.index(simplex)
+      row_counter = 0
+      for lower_d in dic[key -1]:
+         i = allSimplices.index(lower_d)
+         if set(lower_d).issubset(simplex) == True:
+           matrix[i,j] = 1
+           row_counter = allSimplices.index(lower_d)
+      if row_counter != 0:
+         matrix[:,row_counter] = 0
+   key += 1
+   if key == dimension + 1:
+      break
+  return matrix       
+
+#scomplex = [[1,2,3], [3,4], [2,5,7,8,9], [7,8,10], [3,6,7,8,9,10]]
+#scomplex = [[1,2,3]]
+#print(Cmatrixof_simplices(scomplex))
 
 #=============================================
 #Persistence Diagram
 def pers_diagram(scomplex):
-     n = len(listof_simplices(scomplex))
-     list = tdx.barcode(matrixof_simplices(scomplex), n, n)
+     dic = dictof_simplices(scomplex)
+     dimension = max(dic.keys())
+     allSimplices = [sim for k in range(dimension + 1) for sim in dic[k]]
+     n = len(allSimplices)
+     list = tdx.barcode(Cmatrixof_simplices(scomplex), n, n)
      x, y = [], []
      for point in list:
         if len(point) == 2:
@@ -58,11 +104,6 @@ def pers_diagram(scomplex):
      plt.savefig('D:\\figure.jpg', format = 'png', dpi = 300)
      plt.show()
 
-
-scomplex = [[1,2], [2,3,4,5]]
-"""print(listof_simplices(scomplex))
-X = matrixof_simplices(scomplex)
-n = len(listof_simplices(scomplex))
-print(tdx.bar(X,n,n))
-"""
+scomplex = [[1,2], [3,4,10], [2,5,7,8,9], [3,7,8,10], [3,6,7,8,9,10]]
+#x = [comb for sim in scomplex for index in range(1,2) for comb in combinations(sim, index)]
 pers_diagram(scomplex)
